a1d733263085ee66c57855415f85062d58b1fd0a
Ivy <ivyjkemboi@gmail.com>
2024-09-11 13:17:56 +0300

    a bit complete

---
+++ server/migrations/versions/a701f51a5d1a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a701f51a5d1a
+Revises: 569b023af4c3
+Create Date: 2024-09-10 21:05:50.494585
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a701f51a5d1a'
+down_revision = '569b023af4c3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
